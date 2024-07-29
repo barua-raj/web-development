df6d3b1f7d559c731fbd009d21150cb87d135f0a
barua-raj <barua.jovraj@gmail.com>
2024-07-30 01:28:50 +0600

    Delete js directory

--- js/script.js
+++
@@ -1,9 +0,0 @@
-$(".owl-carousel").owlCarousel({
-	autoplay: true,
-	autoplayhoverpause: true,
-	autoplaytimeout: 300,
-	items: 1,
-	loop: true,
-	animateIn: 'flipInX',
-	animateOut: 'zoomOutDown',
-});
