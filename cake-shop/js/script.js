ce8d6f8c0f594e2a5f1f08909453ac4ea6e2673a
barua-raj <barua.jovraj@gmail.com>
2024-07-30 01:28:27 +0600

    Add files via upload

---
+++ cake-shop/js/script.js
@@ -0,0 +1,9 @@
+$(".owl-carousel").owlCarousel({
+	autoplay: true,
+	autoplayhoverpause: true,
+	autoplaytimeout: 300,
+	items: 1,
+	loop: true,
+	animateIn: 'flipInX',
+	animateOut: 'zoomOutDown',
+});
